6b62048dc5bd106b9f5eb70dcef096ac1f1bfb75
surisuryanto <work.surisuryanto@gmail.com>
2022-01-09 01:55:27 +0700

    menambha file theme.dart

---
+++ lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+Color bgColor = const Color(0xff102236);
+Color btnColor = const Color(0xff52AC89);
+Color borderColor = const Color(0xff20435E);
+Color descriptionColor = const Color(0xff82868e);
+Color titleColor = const Color(0xffffffff);
+
+TextStyle titleTextStyle = GoogleFonts.poppins().copyWith(
+  fontWeight: FontWeight.w500,
+  color : titleColor
+);
+
+TextStyle descriptionTextStyle = GoogleFonts.poppins().copyWith(
+  fontWeight: FontWeight.w300,
+  color : descriptionColor
+);
